9dd58954c9afbdd070caa6b597a71b2b114406ad
Your Name <you@example.com>
2019-10-24 20:30:01 -0300

    java

---
+++ projeto.java
@@ -0,0 +1,32 @@
+package br.com.main;
+
+public class projeto {
+
+}
+
+
+
+
+public class Principal {
+	
+	
+	public static void main(String[] args) {
+	
+	
+	Scanner scanner = new Scanner(System.in);
+	double peso;
+	double altura;
+	System.out.println("qual seu peso");
+	peso= scanner.nextDouble();
+	
+	System.out.println("qual sua altura ?");
+	altura= scanner.nextDouble();
+	
+	  
+	
+	
+
+
+	 
+	
+	
